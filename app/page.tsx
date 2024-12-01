74598c6862af088b9dc9eb17b8425ddce88461a4
kohaku-cri <creeperchikin049@gmail.com>
2024-12-01 19:26:21 +0900

    ✨ 新着情報追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import style from "../components/index.module.css"
+import style from "../components/index/index.module.css"
 
 export default function Home() {
   return (
     <>
-    <div>
-      <h1>｜新着情報｜</h1>
+    <div className={style.main}>
+      <h1 className={style.top}>｜新着情報｜</h1>
+      <img src="/img/8.jpg" alt="" />
+      <h2 className={style.topName}>#8魔法使いと空の孤島</h2>
+      <p className={style.sanserif}>ハロウィンマップ第２弾</p>
     </div>
     </>
   );
