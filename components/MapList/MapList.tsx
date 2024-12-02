4939d780e3bd037a5b6ebc7cd322d31f9fbc0ffd
kohaku-cri <creeperchikin049@gmail.com>
2024-12-03 04:11:53 +0900

    ✨ 過去作一覧追加

--- components/MapList/MapList.tsx
+++ components/MapList/MapList.tsx
@@ -5,21 +5,18 @@ import { ListButton } from "../Button/LinkButton";
 
 export function MapList() {
   return (
-    <div>
+    <div className={style.mapList}>
       {mapdata.map((item) => {
         return (
-          <div className={style.mapList}>
-            <div>
-              <Image
-                src={item.mapImgLink}
-                width={imgSize.width}
-                height={imgSize.height}
-                alt={item.alt}
-              />
-              <h2>{item.mapName}</h2>
-              {/* <p>{item.description}</p> */}
-              <ListButton url={item.link}/>
-            </div>
+          <div className={style.map}>
+            <Image
+              src={item.mapImgLink}
+              width={imgSize.width}
+              height={imgSize.height}
+              alt={item.alt}
+            />
+            <h2>{item.mapName}</h2>
+            <ListButton url={item.link} />
           </div>
         );
       })}
