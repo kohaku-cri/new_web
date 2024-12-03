e034d934bbea95b11dc6e538cb5be252cbd00100
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 00:41:27 +0900

    ♻️ 404のリンクボタンにコンポネ使用

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import style from "../components/error404.module.css";
+import { ListButton } from "@/components/Button/LinkButton";
 
 export default function error404() {
   return (
@@ -7,7 +7,7 @@ export default function error404() {
       <h2 className={style.margin}>404Error</h2>
       <h2>ページが見つかりませんでした</h2>
       <p>削除されたか無効なURLです</p>
-      <Link href="/">ホームに戻る</Link>
+      <ListButton url="/">ホームに戻る</ListButton>
     </div>
   );
 }
