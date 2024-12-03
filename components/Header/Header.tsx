b58e4766a24d728de0694142f7d1f4868c0da4bd
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 01:23:05 +0900

    🎨 過去作を情報に置きかえ

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <div className={style.header}>
       <Link href="/" className={style.link}>ホーム</Link>
-      <Link href="" className={style.link}>過去作</Link>
+      <Link href="" className={style.link}>情報</Link>
       <Link href="" className={style.link}>お知らせ</Link>
       <Link href="" className={style.link}>ツール</Link>
 
