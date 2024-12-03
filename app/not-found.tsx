aa0fc540c1931f147c49edc531aef3c24fb8ac17
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 00:17:48 +0900

    🎨 404にリンク追加とデフォルトaタグcssの変更

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import style from "../components/error404.module.css";
 
 export default function error404() {
@@ -6,6 +7,7 @@ export default function error404() {
       <h2 className={style.margin}>404Error</h2>
       <h2>ページが見つかりませんでした</h2>
       <p>削除されたか無効なURLです</p>
+      <Link href="/">ホームに戻る</Link>
     </div>
   );
 }
