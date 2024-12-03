ed739452e03be2345a9655d27c06c0700bbfca9b
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 01:01:25 +0900

    🎨 cssのa要素指定を変更

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,10 +4,10 @@ import style from "../../components/Header/Header.module.css";
 export function Header() {
   return (
     <div className={style.header}>
-      <Link href="/">ホーム</Link>
-      <Link href="">過去作</Link>
-      <Link href="">お知らせ</Link>
-      <Link href="">ツール</Link>
+      <Link href="/" className={style.link}>ホーム</Link>
+      <Link href="" className={style.link}>過去作</Link>
+      <Link href="" className={style.link}>お知らせ</Link>
+      <Link href="" className={style.link}>ツール</Link>
 
     </div>
   );
