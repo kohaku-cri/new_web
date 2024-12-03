25e77514687238599e96b0687056c303ea506b3a
kohaku-cri <creeperchikin049@gmail.com>
2024-12-03 23:49:45 +0900

    ✨ 404エラー実装

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
-import style from "../../components/Header/Header.module.css";
+import style from "../components/error404.module.css";
 
 export default function error404() {
   return (
-    <div>
-      <h2>404</h2>
+    <div className={style.main}>
+      <h2 className={style.margin}>404Error</h2>
+      <h2>ページが見つかりませんでした</h2>
+      <p>削除されたか無効なURLです</p>
     </div>
   );
 }
