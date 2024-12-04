800413113d4ed09ccdf25ccfa1479f22950a5921
kohaku-cri <creeperchikin049@gmail.com>
2024-12-05 01:50:27 +0900

    🐛 正しく新着順となるように修正

--- public/dataBase/newsData.ts
+++ public/dataBase/newsData.ts
@@ -3,6 +3,6 @@ type data = {
   date: string;
 };
 export const newsData: data[] = [
-  { title: "サイトの更新情報まとめ", date: "2024-12-04-01" },
   { title: "もういくつ寝ると～クリスマス～", date: "2024-12-04-02" },
+  { title: "サイトの更新情報まとめ", date: "2024-12-04-01" },
 ];
