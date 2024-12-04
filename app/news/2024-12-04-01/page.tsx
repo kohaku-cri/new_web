4bc80480d5e93eb18db38005bbe4d763ad3dbe37
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 21:55:08 +0900

    ✨ 記事名を変数として記事に表示

--- app/news/2024-12-04-01/page.tsx
+++ app/news/2024-12-04-01/page.tsx
@@ -2,15 +2,17 @@
 import { ListButton } from "@/components/Button/LinkButton";
 import style from "@/components/news.module.css";
 import { newsData } from "@/public/dataBase/newsData";
-import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function News() {
-
+  const pathName = usePathname();
+  const foundData = newsData.find((item) => "/news/" + item.date === pathName);
+  
   return (
     <div className={style.main}>
       <div>
         <div>
-          <h2>｜タイトル</h2>
+          <h2>｜{foundData?.title}</h2>
           <p>サイトの更新を行いました 変更点は以下の通りです</p>
           <h3>
             A. <span className={style.lightBlue}>追加</span>
--- public/dataBase/newsData.ts
+++ public/dataBase/newsData.ts
@@ -3,6 +3,6 @@ type data = {
   date: string;
 };
 export const newsData: data[] = [
-  { date: "2024-12-04-01", title: "もうすぐクリスマスですね！" },
-  { date: "2024-12-04-02", title: "サイトの更新情報まとめ" },
+  { date: "2024-12-04-01", title: "サイトの更新情報まとめ" },
+  { date: "2024-12-04-02", title: "もういくつ寝ると～クリスマス～" },
 ];
