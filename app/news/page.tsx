797d7fa6cc47370af8f765952af58dde7f2af27b
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 22:55:07 +0900

    ✨ お知らせ一覧を追加(必デザイン修正)

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,11 +1,14 @@
 import { ListButton } from "@/components/Button/LinkButton";
-import style from "@/components/newsList.module.css"
-
+import style from "@/components/newsList.module.css";
+import { newsData } from "@/public/dataBase/newsData";
+import Link from "next/link";
 
 export default function New() {
-  return (
-    <div className={style.main}>
-        
-    </div>
-  );
+  return <div className={style.main}>
+    {newsData.map((item)=>{
+      return (
+        <Link href={`/news/${item.date}`}>{item.title}</Link>
+      )
+    })}
+  </div>;
 }
