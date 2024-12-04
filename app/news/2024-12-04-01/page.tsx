f05936e07227b46672fe35df41048ce80d13c9e0
kohaku-cri <creeperchikin049@gmail.com>
2024-12-04 22:11:24 +0900

    🎨 細かい修正

--- app/news/2024-12-04-01/page.tsx
+++ app/news/2024-12-04-01/page.tsx
@@ -7,24 +7,18 @@ import { usePathname } from "next/navigation";
 export default function News() {
   const pathName = usePathname();
   const foundData = newsData.find((item) => "/news/" + item.date === pathName);
-  
+
   return (
     <div className={style.main}>
       <div>
         <div>
           <h2>｜{foundData?.title}</h2>
           <p>サイトの更新を行いました 変更点は以下の通りです</p>
-          <h3>
-            A. <span className={style.lightBlue}>追加</span>
-          </h3>
+          <h3 className={style.lightBlue}>〇 追加</h3>
           <p>・お知らせページ</p>
-          <h3>
-            B. <span className={style.green}>変更</span>
-          </h3>
+          <h3 className={style.green}>〇 変更</h3>
 
-          <h3>
-            C. <span className={style.red}>削除</span>
-          </h3>
+          <h3 className={style.red}>〇 削除</h3>
           <p>・隠れクリーパー</p>
         </div>
       </div>
