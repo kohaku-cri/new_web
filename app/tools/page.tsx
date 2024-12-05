f6abefd065ff9b20f40fc3e4229d122f651de8b9
kohaku-cri <creeperchikin049@gmail.com>
2024-12-05 17:24:55 +0900

    ✨ toolsページを追加

---
+++ app/tools/page.tsx
@@ -0,0 +1,9 @@
+import style from "@/components/Tools/tools.module.css";
+
+export default function Tools() {
+  return (
+    <div className={style.main}>
+      <h2>このページは現在準備中です...</h2>
+    </div>
+  );
+}
