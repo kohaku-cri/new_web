bed856212fe2723b109cb24be48a619919c46b0b
kohaku-cri <creeperchikin049@gmail.com>
2024-12-06 03:48:24 +0900

    💩 ワールド紹介ページ

--- app/world/8/page.tsx
+++ app/world/8/page.tsx
@@ -1,9 +1,36 @@
-import style from "@/components/World/world.module.css"
+import style from "@/components/World/world.module.css";
+import { mapdata, imgSize } from "@/public/dataBase/MapData";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function World() {
   return (
     <div className={style.main}>
-      <iframe className={style.youtube} src="https://www.youtube.com/embed/nmF8JFw_GZI?si=j14rS4wTPfA5pnOT" allowFullScreen></iframe>
+      <div className={style.test}>
+        <iframe
+          className={style.youtube}
+          src="https://www.youtube.com/embed/nmF8JFw_GZI?si=j14rS4wTPfA5pnOT"
+          allowFullScreen
+        ></iframe>
+
+        <div className={style.otherMap}>
+          {mapdata.map((item) => {
+            return (
+              <Link href={item.link}>
+                <div>
+                  <Image
+                    src={item.mapImgLink}
+                    alt={item.alt}
+                    width="180"
+                    height="101"
+                  />
+                  <h3>{item.mapName}</h3>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
