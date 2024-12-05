5079dda81228c7ebd48d2f0dc1be1e3d202323ee
kohaku-cri <creeperchikin049@gmail.com>
2024-12-05 18:44:14 +0900

    ✨ 情報ページ追加

---
+++ app/information/page.tsx
@@ -0,0 +1,9 @@
+import style from "@/components/information/information.module.css";
+
+export default function Tools() {
+  return (
+    <div className={style.main}>
+      <h2>このページは現在準備中です...</h2>
+    </div>
+  );
+}
