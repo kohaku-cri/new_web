5de628b6ce8cc33216e9e46d34cb28b93ad393a6
kohaku-cri <creeperchikin049@gmail.com>
2024-12-05 15:44:54 +0900

    📝 編集

--- app/news/2024-12-04-01/page.tsx
+++ app/news/2024-12-04-01/page.tsx
@@ -17,9 +17,16 @@ export default function News() {
           <h3 className={style.lightBlue}>〇 追加</h3>
           <p>・お知らせページ</p>
           <h3 className={style.green}>〇 変更</h3>
-
+          <p>・全体的なデザイン</p>
           <h3 className={style.red}>〇 削除</h3>
           <p>・隠れクリーパー</p>
+          <p>・財宝ナビゲータ（一時的）</p>
+          <h2 className={style.marTop200}>技術的なお話</h2>
+          <h3 className={style.lightBlue}>〇 追加</h3>
+          <p>・Next.js （React）</p>
+          <h3 className={style.red}>〇 削除</h3>
+          <p>・直に記述するHTML</p>
+          <p className={style.marTop110}>せっかくなので何が変わったのかについて少しまとめてみたいと思います</p>
         </div>
       </div>
     </div>
