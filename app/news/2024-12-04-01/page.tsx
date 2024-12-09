53cce1633e04d5765d168fe3b1e567f9afa044f0
kohaku-cri <creeperchikin049@gmail.com>
2024-12-09 18:46:21 +0900

    📝

--- app/news/2024-12-04-01/page.tsx
+++ app/news/2024-12-04-01/page.tsx
@@ -15,8 +15,11 @@ export default function News() {
           <p>サイトの更新を行いました 変更点は以下の通りです</p>
           <h3 className={style.lightBlue}>〇 追加</h3>
           <p>・お知らせページ</p>
+          <p>・公開状況の明確化</p>
+          <p>・各ワールド紹介ページに権利情報を追加</p>
           <h3 className={style.green}>〇 変更</h3>
           <p>・全体的なデザイン</p>
+          <p>・ガイドラインをもっと見やすく</p>
           <h3 className={style.red}>〇 削除</h3>
           <p>・隠れクリーパー</p>
           <p>・財宝ナビゲータ（一時的）</p>
