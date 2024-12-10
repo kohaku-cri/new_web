57905ef4d26b45757b2159d2ad950e0959ea0019
kohaku-cri <creeperchikin049@gmail.com>
2024-12-11 04:08:40 +0900

    🚧 記事の順番

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,15 +1,31 @@
+"use client";
 import style from "@/components/newsList.module.css";
 import { newsData } from "@/public/dataBase/newsData";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function News() {
+  const [order, setorder] = useState(false);
+  const buttonOrder = () => {
+    setorder((i) => !i);
+  };
   return (
     <div className={style.main}>
-      <h2>新着順</h2>
+      <div className={style.topTitle}>
+        <h2 className={style.changeOrder} onClick={buttonOrder}>
+          {order ? "⇧" : "⇩"}
+        </h2>
+        <h2 className={style.title}>新着順</h2>
+        <h2 className={style.changeOrder} onClick={buttonOrder}>
+          {order ? "⇧" : "⇩"}
+        </h2>
+      </div>
       {newsData.map((item) => {
         return (
           <div key={item.title}>
-            <Link href={`/news/${item.date}`}>{item.date}・{item.title}</Link>
+            <Link href={`/news/${item.date}`}>
+              {item.date}・{item.title}
+            </Link>
           </div>
         );
       })}
