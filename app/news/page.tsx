72976310af1108afcb5f85a3c9d94d84cb92e0bf
kohaku-cri <creeperchikin049@gmail.com>
2024-12-11 18:44:45 +0900

    ✨ 順番変更 実装

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -5,22 +5,24 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function News() {
-  const [order, setorder] = useState(false);
+  const [order, setorder] = useState(true);
   const buttonOrder = () => {
     setorder((i) => !i);
   };
+  const data = order ? newsData : newsData.slice().reverse();
+
   return (
     <div className={style.main}>
       <div className={style.topTitle}>
         <h2 className={style.changeOrder} onClick={buttonOrder}>
           {order ? "⇧" : "⇩"}
         </h2>
-        <h2 className={style.title}>新着順</h2>
+        <h2 className={style.title}>{order ? "新しい順" : "古い順"}</h2>
         <h2 className={style.changeOrder} onClick={buttonOrder}>
           {order ? "⇧" : "⇩"}
         </h2>
       </div>
-      {newsData.map((item) => {
+      {data.map((item) => {
         return (
           <div key={item.title}>
             <Link href={`/news/${item.date}`}>
