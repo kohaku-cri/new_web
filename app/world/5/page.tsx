e1ea362bdd375a923de789bdacbac96b7e06d665
kohaku-cri <creeperchikin049@gmail.com>
2024-12-15 18:16:15 +0900

    📝

--- app/world/5/page.tsx
+++ app/world/5/page.tsx
@@ -1,3 +1,4 @@
+import { ListButton } from "@/components/Button/LinkButton";
 import World from "@/components/World/World";
 
 export default function Worlds() {
@@ -5,7 +6,16 @@ export default function Worlds() {
     <World
       explain={
         //ここに概要欄の内容を入力 JSX（ヒント＆答えは別箇所）
-        <></>
+        <>
+          {" "}
+          <h3>ヒント＆答え</h3>
+          <p>Googleサイトにて公開中</p>
+          <ListButton
+            url="https://sites.google.com/view/kohaku-datsusyutu/%E3%83%8F%E3%83%AD%E3%82%A6%E3%82%A3%E3%83%B3%E3%83%8F%E3%82%A6%E3%82%B9%E3%81%8B%E3%82%89%E3%81%AE%E8%84%B1%E5%87%BA"
+            title="ヒントはこちら"
+            visibility=""
+          />
+        </>
       }
       //ヒントは配列内のオブジェクトに記述
       //title・・・常に表示される文
--- app/world/6/page.tsx
+++ app/world/6/page.tsx
@@ -1,3 +1,4 @@
+import { ListButton } from "@/components/Button/LinkButton";
 import World from "@/components/World/World";
 
 export default function Worlds() {
@@ -5,7 +6,15 @@ export default function Worlds() {
     <World
       explain={
         //ここに概要欄の内容を入力 JSX（ヒント＆答えは別箇所）
-        <></>
+        <>
+          <h3>ヒント＆答え</h3>
+          <p>Googleサイトにて公開中</p>
+          <ListButton
+            url="https://sites.google.com/view/kohaku-datsusyutu/%E3%82%AF%E3%83%AA%E3%82%B9%E3%83%9E%E3%82%B9%E3%83%9E%E3%83%83%E3%83%97"
+            title="ヒントはこちら"
+            visibility=""
+          />
+        </>
       }
       //ヒントは配列内のオブジェクトに記述
       //title・・・常に表示される文
