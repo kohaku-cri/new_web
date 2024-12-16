7d7e25d165d19d005ec32782aad74514c69e8711
kohaku-cri <creeperchikin049@gmail.com>
2024-12-17 00:50:49 +0900

    🚀 2

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/new_web",
-  assetPrefix: "/new_web",
 };
 
 export default nextConfig;
