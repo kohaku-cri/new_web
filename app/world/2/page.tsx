79ffe3fba87ff282ccf25a45dc9f4bb1289f5289
kohaku-cri <creeperchikin049@gmail.com>
2025-01-25 04:40:34 +0900

    🚧 全てのページにpropsを追加

--- app/world/2/page.tsx
+++ app/world/2/page.tsx
@@ -17,6 +17,13 @@ export default function Worlds() {
 
       //img・・・画像のリンクを入力（省略可能）　※imgの使用にはaltの入力が必要
       hint={[]}
+      // 空配列の場合は情報は「修正されたバグはありません」と表示される
+      // title バグ修正の概要
+      // date 修正日
+      // importance バグの重要度（A＝非常に重大、B＝通常の問題、C＝軽微な問題）
+      bugsFix={[
+        {title:"民家の隣の小道から未解放エリアへ行けてしまう問題",date:"不明",importance:"A"},
+      ]}
     />
   );
 }
